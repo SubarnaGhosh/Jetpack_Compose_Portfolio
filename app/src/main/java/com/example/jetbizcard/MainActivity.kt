15002c041384415ce5921a33c6b2e8bc1bd6cd37
SubarnaGhosh <subarnaghosh329@gmail.com>
2024-04-22 21:54:17 +0530

    First

---
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -0,0 +1,198 @@
+package com.example.jetbizcard
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetbizcard.ui.theme.JetBizCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetBizCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                        createBizCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun createBizCard(){
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
+        Surface(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()) {
+            Card(modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+                shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            ) {
+                Column(modifier = Modifier.width(390.dp),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CreateImageProfile()
+                    CreateInfo()
+                    Button(onClick = {
+                        buttonClickedState.value = !buttonClickedState.value
+                    }
+                    ) {
+                        Text(text = "Portfolio",
+                            style = MaterialTheme.typography.titleMedium)
+                    }
+                    if(buttonClickedState.value){
+                        Content()
+                    }else{
+                        Box() {}
+                    }
+                }
+
+            }
+        }
+}
+
+@Composable
+fun Content() {
+    Box(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(5.dp)) {
+        Surface(modifier = Modifier
+            .padding(3.dp)
+            .fillMaxSize(),
+//            .fillMaxWidth()
+//            .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(2.dp ,Color.LightGray)
+        ) {
+                Portfolio(data = listOf("Project 1", "Project 2" , "Project 3" , "Project 4" , "Project 4" , "Project 4"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn{
+        items(data) {
+            item ->
+            Card(modifier = Modifier
+                .padding(13.dp)
+                .fillMaxSize(),
+                shape = RectangleShape){
+                Row(modifier = Modifier
+                    .padding(8.dp)
+                    .background(MaterialTheme.colorScheme.surface)
+                    .padding(7.dp)){
+                    CreateImageProfile(modifier = Modifier.size(100.dp))
+                    Column(modifier = Modifier.padding(7.dp)
+                        .align(alignment = Alignment.CenterVertically)) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A great project")
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun CreateInfo() {
+    Divider()
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Subarna Ghosh",
+            style = MaterialTheme.typography.displayMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Android Developer",
+            modifier = Modifier.padding(3.dp)
+        )
+        Text(
+            text = "subarnaghosh329@gmail.com",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.labelMedium
+        )
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        shadowElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "profile image",
+            modifier = modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetBizCardTheme {
+        createBizCard()
+    }
+}
